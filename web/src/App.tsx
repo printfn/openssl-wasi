aae22948f32593da8ebd2c08d1ef56c556f6681e
printfn <printfn@users.noreply.github.com>
2024-04-07 01:04:26 +0000

    Add PKCS #7

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import { execute } from './openssl';
 import { Container } from 'react-bootstrap';
 import { Buffer } from 'node:buffer';
 
-type FileType = 'cert' | 'csr' | 'crl';
+type FileType = 'cert' | 'csr' | 'crl' | 'pkcs#7';
 
 function getCommand(fileType: FileType, pem: boolean) {
 	const fmt = pem ? 'PEM' : 'DER';
@@ -14,6 +14,8 @@ function getCommand(fileType: FileType, pem: boolean) {
 			return ['req', '-in', 'inputfile', '-inform', fmt, '-text', '-noout'];
 		case 'crl':
 			return ['crl', '-in', 'inputfile', '-inform', fmt, '-text', '-noout'];
+		case 'pkcs#7':
+			return ['pkcs7', '-in', 'inputfile', '-inform', fmt, '-print', '-noout'];
 		default:
 			throw new Error('unknown file type ' + fileType);
 	}
@@ -49,31 +51,42 @@ function App() {
 	return (
 		<Container>
 			<h1>Decode ASN1</h1>
-			<textarea
-				style={{ width: '100%', height: '10rem' }}
-				onChange={e => setFile(new TextEncoder().encode(e.currentTarget.value))}
-				value={
-					pem
-						? new TextDecoder().decode(file)
-						: Buffer.from(file).toString('base64')
-				}
-			/>
-			<input type="file" onChange={e => decodeFile(e.currentTarget.files)} />
-			<input
-				type="checkbox"
-				id="is-pem"
-				checked={pem}
-				onChange={e => setPEM(e.currentTarget.checked)}
-			/>{' '}
-			<label htmlFor="is-pem">PEM</label>
-			<select
-				value={fileType}
-				onChange={e => setFileType(e.currentTarget.value as FileType)}
-			>
-				<option value="cert">Certificate</option>
-				<option value="csr">Certificate Signing Request</option>
-				<option value="crl">Certificate Revocation List</option>
-			</select>
+			<p>
+				<textarea
+					style={{ width: '100%', height: '10rem' }}
+					onChange={e =>
+						setFile(
+							pem
+								? new TextEncoder().encode(e.currentTarget.value)
+								: new Uint8Array(Buffer.from(e.currentTarget.value, 'base64')),
+						)
+					}
+					value={
+						pem
+							? new TextDecoder().decode(file)
+							: Buffer.from(file).toString('base64')
+					}
+				/>
+				<input type="file" onChange={e => decodeFile(e.currentTarget.files)} />
+			</p>
+			<p>
+				<select
+					value={pem ? 'pem' : 'der'}
+					onChange={e => setPEM(e.currentTarget.value === 'pem')}
+				>
+					<option value="pem">PEM</option>
+					<option value="der">DER</option>
+				</select>
+				<select
+					value={fileType}
+					onChange={e => setFileType(e.currentTarget.value as FileType)}
+				>
+					<option value="cert">Certificate</option>
+					<option value="csr">Certificate Signing Request</option>
+					<option value="crl">Certificate Revocation List</option>
+					<option value="pkcs#7">PKCS #7</option>
+				</select>
+			</p>
 			<pre>{decoded}</pre>
 		</Container>
 	);
