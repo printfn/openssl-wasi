258b76316531e985e8b6bdd5ec46212aaff66762
printfn <printfn@users.noreply.github.com>
2025-03-21 08:27:06 +0000

    Add SLH-DSA

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -27,6 +27,7 @@ const FileTypes = [
 	'create-rsa',
 	'create-ecc',
 	'create-mldsa',
+	'create-slhdsa',
 	'create-digest',
 	'pkcs7-certs-crls',
 	'pkcs12-certs',
@@ -54,11 +55,13 @@ function getCommand(fileType: FileType, pem: boolean) {
 		case 'create-cert':
 			return `openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -keyout private.key -out certificate.crt -verbose -sha256 -days 7 -noenc -subj "/CN=example.com" -text -addext "subjectAltName=DNS:example.com"`;
 		case 'create-rsa':
-			return `openssl genpkey -algorithm rsa -pkeyopt bits:2048 -out private.key`;
+			return `openssl genpkey -algorithm rsa -pkeyopt bits:2048 -out private.key -outpubkey public.key`;
 		case 'create-ecc':
-			return `openssl genpkey -algorithm ec -pkeyopt group:secp384r1 -out private.key`;
+			return `openssl genpkey -algorithm ec -pkeyopt group:secp384r1 -out private.key -outpubkey public.key`;
 		case 'create-mldsa':
-			return `openssl genpkey -algorithm mldsa65 -provparam ml-dsa.output_formats=seed-only -out private.key`;
+			return `openssl genpkey -algorithm mldsa65 -provparam ml-dsa.output_formats=seed-only -out private.key -outpubkey public.key`;
+		case 'create-slhdsa':
+			return `openssl genpkey -algorithm SLH-DSA-SHA2-192s -out private.key -outpubkey public.key`;
 		case 'create-digest':
 			return `openssl dgst -sha256 -out signature.bin input_file`;
 		case 'pkcs7-certs-crls':
@@ -161,6 +164,7 @@ function App() {
 					</optgroup>
 					<optgroup label="Create">
 						<option value="create-mldsa">ML-DSA Private Key</option>
+						<option value="create-slhdsa">SLH-DSA Private Key</option>
 						<option value="create-ecc">ECC Private Key</option>
 						<option value="create-rsa">RSA Private Key</option>
 						<option value="create-csr">Certificate Signing Request</option>
